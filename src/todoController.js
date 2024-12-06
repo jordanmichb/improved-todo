30a0331c723e5642b9bb5029e88f5fde78a430a8
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-12-06 13:25:46 -0800

    Set some example tasks to use today's date
    Use Date() to set example tasks to use today's
    date and dates within the week so they will show
    in the 'today' and 'completed' tabs.

--- src/todoController.js
+++ src/todoController.js
@@ -18,20 +18,23 @@ const TodoController = (function() {
     function setProjects() {
         if (!StorageController.get('projects')) {
             const fakeProject = new Project('Project1', '11/8/2024');
+            const today = new Date();
+            //const today = `${d.getMonth()}/${d.getDate()}/${d.getFullYear()}`;
+
             fakeProject.addTask('This task name is going to be really really really long as an example to show how it will display on screen', 
                 'This task description is going to be really really really long as an example to show how it will display on screen. ' +
                 'Lorem ipsum odor amet, consectetuer adipiscing elit. Vitae velit parturient nullam dictum quisque in. Pharetra malesuada morbi primis diam ex sollicitudin! Placerat laoreet nisi congue taciti neque ante pulvinar at. Finibus fames blandit varius, nulla suscipit suspendisse sapien. Tincidunt ad libero mollis accumsan pellentesque mollis condimentum. Molestie sed fusce arcu orci posuere tortor faucibus hac. Mollis cras ullamcorper urna; egestas montes nulla vehicula sapien primis. Sit at molestie dapibus fames mi semper. Habitant faucibus dapibus convallis nec massa per. Quis in consectetur accumsan amet pharetra risus magna. Rutrum sapien eleifend netus placerat vestibulum mollis.', 
                 '11/14/2024', 'priority');
-            fakeProject.addTask('Task1.2', 'description', '12/15/2024', 'priority', true);
+            fakeProject.addTask('Task1.2', 'description', `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`, 'priority');
             fakeProject.addTask('Task1.3', 'description', '11/22/2024', 'priority');
-            fakeProject.addTask('Task1.4', 'description', '11/22/2024', 'priority');
+            fakeProject.addTask('Task1.4', 'description', `${today.getMonth() + 1}/${today.getDate() + 5}/${today.getFullYear()}`, 'priority');
             projects.push(fakeProject);
     
             const fakeProject2 = new Project('Project2');
-            fakeProject2.addTask('Task2', 'description', '11/21/2024', 'priority');
-            fakeProject2.addTask('Task2.1', 'description', '11/22/2025', 'priority');
+            fakeProject2.addTask('Task2', 'description', `${today.getMonth() + 1}/${today.getDate()}/${today.getFullYear()}`, 'priority');
+            fakeProject2.addTask('Task2.1', 'description', `${today.getMonth() + 1}/${today.getDate() + 2}/${today.getFullYear()}`, 'priority');
             fakeProject2.addTask('Task2.2', 'description', '11/20/2024', 'priority');
-            fakeProject2.addTask('Task2.3', 'description', '11/14/2024', 'priority');
+            fakeProject2.addTask('Task2.3', 'description', `${today.getMonth() + 1}/${today.getDate() + 7}/${today.getFullYear()}`, 'priority');
             projects.push(fakeProject2);
             StorageController.setAsString('projects', projects);
         }
